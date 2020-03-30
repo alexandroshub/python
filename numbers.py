3639988a7c0b5256832ce49e6f048d4c590f3e38
Alexandros P <alexispapy@gmail.com>
2020-03-30 20:47:39 +0300

    first commit

---
+++ numbers.py
@@ -0,0 +1,16 @@
+# Papaspyrou Alexandros 30/3/20
+x=input('Δώσε 1ο αριθμό: ')
+y=input('Δώσε 2ο αριθμό: ')
+x = int(x)
+y = int(y)
+s=x+y
+m=x*y
+print("\nΆθροισμα: {}\nΓινόμενο: {}".format(s,m))
+
+C = input("\nδώσε θερμοκρασία σε βαθμούς Κελσίου:")
+C = float(C)
+F = C*1.8+32
+print("Η θερμοκρασία σε βαθμούς Φαρενάιτ είναι {:1.2f}" .format(F))
+
+st = 23*"="+"\n"+"alexandroshub.github.io\n"+ 23*"="
+print(st)
